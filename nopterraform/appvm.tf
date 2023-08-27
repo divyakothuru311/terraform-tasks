bc13246047e9e5ea06bfcd80bb469c1f901a7be5
divya <divyakothuru311@gmail.com>
2023-08-27 17:53:00 +0530

    added

--- nopterraform/appvm.tf
+++ nopterraform/appvm.tf
@@ -5,52 +5,88 @@ resource "aws_key_pair" "idrsa" {
     CreatedBy = "terraform"
   }
 }
-data "aws_subnet" "app" {
-  filter {
-    name   = "tag:Name"
-    values = [var.app_subnet_name]
-  }
+# data "aws_subnet" "app" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["app"]
+#   }
+#   depends_on = [aws_subnet.subnets]
+# }
+# data "aws_subnet" "app1" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["app1"]
+#   }
 
-  depends_on = [
-    aws_subnet.subnets
-  ]
-}
+
+#   depends_on = [
+#     aws_subnet.subnets
+#   ]
+# }
+
 resource "aws_instance" "appserver" {
-  ami                         = var.ubuntu_ami_id
+  count                       = length(var.ec2_names)
+  ami                         = var.ubuntu_ami_id[count.index]
   associate_public_ip_address = true
-  instance_type               = var.app_ec2_size
+  instance_type               = var.app_ec2_size[count.index]
   key_name                    = aws_key_pair.idrsa.key_name
   vpc_security_group_ids      = [aws_security_group.app.id]
-  subnet_id                   = data.aws_subnet.app.id
+  subnet_id                   = aws_subnet.subnets.*.id[count.index]
+  // user_data                   = file(var.ansiblescript)
   tags = {
-    Name = "appserver"
+    Name = var.ec2_names[count.index]
   }
+  provisioner "local-exec" {
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file(var.private_key_path)
-    host        = self.public_ip
+    command = "terraform output -raw nop_url > hosts"
+
   }
+  provisioner "local-exec" {
 
-  #provisioner "file" {
-  # source      = "./installansible.sh"
-  #destination = "/tmp/installansible.sh"
+    command = "terraform output -raw nop1_url > hosts"
 
-  #}
+  }
+   provisioner "local-exec" {
 
-  provisioner "remote-exec" {
-    #script = "/tmp/installansible.sh"
-    inline = [
-      "sudo apt update",
-      "sudo apt install software-properties-common -y",
-      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
-      "sudo apt install ansible -y",
-    "ansible-playbook -i hosts nop-playbook.yaml"]
+    command = "ansible-playbook -i hosts nop-playbook.yaml"
 
   }
 
-  depends_on = [
-    aws_subnet.subnets
-  ]
 }
+
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"
+#     private_key = file(var.private_key_path)
+#     host        = self.public_ip
+#   }
+
+
+
+#   provisioner "file" {
+#     source      = "nop-playbook.yaml"
+#     destination = "/tmp/nop-playbook.yaml"
+#   }
+#   provisioner "file" {
+#     source      = "nopcommerce.service"
+#     destination = "/tmp/nopcommerce.service"
+#   }
+#   provisioner "file" {
+#     source      = "default"
+#     destination = "/tmp/default"
+#   }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo apt update",
+#       "sudo apt install software-properties-common -y",
+#       "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+#       "sudo apt install ansible -y",
+#       "ansible-playbook -i hosts /tmp/nop-playbook.yaml"
+#     ]
+#   }
+
+
+#   depends_on = [
+#     aws_subnet.subnets
+#   ]
+# }
